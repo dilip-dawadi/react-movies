a284c0253f11315826a3233d96434a316eddd68d
dilip-dawadi <zanzerdawadi123@gmail.com>
2025-04-08 19:03:55 -0400

    Add dotenv dependency and configure database connection in application properties

--- backend/src/main/java/simpleapi/demo/DemoApplication.java
+++ backend/src/main/java/simpleapi/demo/DemoApplication.java
@@ -2,11 +2,17 @@ package simpleapi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
